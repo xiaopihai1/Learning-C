842df473ca81809e5380de9370564cf4f2b56e8d
young <18796102266@163.com>
2021-11-19 15:17:28 +0800

    02

---
+++ Assignment-02/study.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    double b = 3.14;
+    char c[] = "hello world";
+    printf("%d \n",a);
+    printf("%f \n",b);
+    printf("%s \n",c);
+    return 0;
+}
